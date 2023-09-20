2aabfe19c1426b898319ac93ca42d8e18e6beba9
xurshidbek <shamsun.com@gmail.com>
2023-09-20 08:52:55 +0500

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO country (name) VALUES ('India');
+INSERT INTO country (name) VALUES ('Brazil');
+INSERT INTO country (name) VALUES ('USA');
+INSERT INTO country (name) VALUES ('Italy');
+INSERT INTO country (name) VALUES ('Uzbekistan');
+INSERT INTO country (name) VALUES ('Turkiye');
